cac5c063e894f55efb06dfec6444d019eaa33596
Elizabeth Codd <elizabethcodd@Elizabeths-MacBook-Pro.local>
2019-03-12 16:49:13 +0000

    make a simple HTML input for links

---
+++ front-end/index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+  <title>Document</title>
+</head>
+<body>
+  <label for="link">Type your link here:</label>
+  <input type="text" required/>
+  <button class='button' type='button' id='submitButton'></button>
+</body>
+</html>
